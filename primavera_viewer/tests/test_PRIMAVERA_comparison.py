9706be2a7f4c82a312e1f053f4366cbd0d6e24a9
Philip Rutter <rutter_100@outlook.com>
2018-07-17 15:50:54 +0100

    Included two testing modules. One for entire PRIMAVERA comparison (mainly used for operation from python interpreter with no actual testing) and an actual unit test verifying the method used to find the nearest known point.

--- primavera_viewer/tests/test_PRIMAVERA_comparison.py
+++ primavera_viewer/tests/test_PRIMAVERA_comparison.py
@@ -15,74 +15,50 @@ Version input options: ['gn/v20180605', 'gn/v20170808', 'gn/v20170831',
 import unittest
 from primavera_viewer.get_PRIMAVERA_data import *
 from primavera_viewer.experiments_data import *
+from primavera_viewer.cube_statistics import all_experiments_mean
 
 class TestPrimaveraViewer(unittest.TestCase):
 
     def setUp(self):
         self.time_constraints = [1950, 1960]  # input start/end year
-        self.location_point = [20.2, 30.5]
+        self.location_point = [20.2, 23.2, 30.5, 34.9]
         self.choose_constraints = [self.time_constraints]
-        self.experiment_inputs = Experiments(['tasmax'], 
+        self.experiment_inputs = Experiments(['tasmax'],
                                              ['MOHC/HadGEM3-GC31-LM',
                                              'CMCC/CMCC-CM2-HR4',
                                              'EC-Earth-Consortium/EC-Earth3',
-                                             'ECMWF/ECMWF-IFS-LR'], 
-                                             ['r1i1p1f1'], 
+                                             'ECMWF/ECMWF-IFS-LR'],
+                                             ['r1i1p1f1'],
                                              ['gn/v20180605', 'gr/v20170911',
                                                'gn/v20170706', 'gr/v20170915'])
         self.plot_type = 'annual_mean_timeseries'
-        
+
     def test_experiment_comparison(self):
+
         experiments_list = self.experiment_inputs\
             .load_all_data('concatenate', self.choose_constraints)
-        experiments_data = ExpStatistics(experiments_list, 
-                                         loc=self.location_point) \
-            .constrain_location()
-        experiments_mean = ExpStatistics(experiments_data, 
-                                         loc=self.location_point) \
-            .all_experiments_mean()
-        result = ExpPlotting(experiments_data, experiments_mean, self.plot_type)
+
+        # Create class for experiment data at requested location
+        experiments_data = ExperimentsData(experiments_list,
+                                           loc=self.location_point)
+
+        # Unify experiment spacial coordinate systems
+        ed_unified_spatial_coords = experiments_data.unify_spatial_coordinates()
+
+        # Subset experiment data at requested location
+        ed_constrained_location = ed_unified_spatial_coords.constrain_location()
+
+        # Unify format of experiment data (time dimension)
+        ed_unified = ed_constrained_location.unify_cube_format()
+
+        experiments_mean = all_experiments_mean(ed_unified
+                                                .experiments_list)
+
+        result = ExperimentsPlot(ed_unified.experiments_list, experiments_mean,
+                             self.plot_type)
         result.plot_all()
 
 
 if __name__ == '__main__':
     unittest.main()
-    
-    
-    
-    
-# time_constraints = [1950, 1960]  # input start/end year
-# location_point = [30.00, 34.45, 190.78, 198.61]    # define point location
-# 
-# # |---- Option for constrained loading instead of post load subsetting ----|
-# # lat_constraints = [60, 60]  # define latitude bounds
-# # lon_constraints = [30, 30]  # define longitude bounds
-# choose_constraints = [time_constraints]#, lat_constraints, lon_constraints]
-# 
-# # Create class containing details of all experiments
-# experiment_inputs = Experiments(['tasmax'], ['MOHC/HadGEM3-GC31-LM',
-#                                              'CMCC/CMCC-CM2-HR4',
-#                                              'EC-Earth-Consortium/EC-Earth3',
-#                                              'ECMWF/ECMWF-IFS-LR'],
-#                                 ['r1i1p1f1'], ['gn/v20180605', 'gr/v20170911',
-#                                                'gn/v20170706', 'gr/v20170915'])
-# plot_type = 'annual_mean_timeseries'
-# 
-# # Load with constraints and choose to concatenate all the required files
-# experiments_list = experiment_inputs.load_all_data('concatenate',
-#                                                    choose_constraints)
-# print('finished load')
-# # Subset all cubes with requested location
-# experiments_data = ExpStatistics(experiments_list, loc=location_point)\
-#     .constrain_location()
-# 
-# # Merge multi-model/ensemble cubes and produce a mean for each time point
-# experiments_mean = ExpStatistics(experiments_data, loc=location_point) \
-#     .all_experiments_mean()
-# 
-# # Create class containing data from all experiments, the experiment mean
-# # timeseries and the requested plot type
-# result = ExpPlotting(experiments_data, experiments_mean, plot_type)
-# 
-# # Plot the data as requested
-# result.plot_all()
+
--- primavera_viewer/tests/test_nearest_location.py
+++ primavera_viewer/tests/test_nearest_location.py
@@ -1,22 +1,72 @@
 """
 Tests for primavera_viewer
 """
-
-import iris
 import unittest
 from iris.tests.stock import realistic_3d
 from primavera_viewer.nearest_location import *
 
-from primavera_viewer.nearest_location import *
-
 class TestPointLocation(unittest.TestCase):
 
-    def setup(self):
-        self.example_cube = realistic_3d()
-        self.example_cube.coord('gird_latitude').guess_bounds()
-        self.example_cube.coord('gird_latitude').guess_bounds()
-        self.latitude = 1.13
-        self.longitude = 1.45
+    def setUp(self):
+        self.stock_cube = realistic_3d()
+        self.stock_cube.coord('grid_latitude').guess_bounds()
+        self.stock_cube.coord('grid_longitude').guess_bounds()
 
-    def test_find_point(self):
-        test_point = PointLocation()
+    def test_find_point_general_case(self):
+        """
+        Tests general case of single point within coordinate system
+        """
+        self.test_cube = PointLocation(lat=-1.51,lon=3.78,cube=self.stock_cube)
+        self.lat_coord = self.test_cube.find_point().coord('latitude')
+        self.lon_coord = self.test_cube.find_point().coord('longitude')
+        self.assertEqual([self.lat_coord.points[0], self.lon_coord.points[0]],
+                         [-2.0, 4.0])
+
+    def test_find_point_on_boundary_case(self):
+        """
+        Tests case of single point on boundary between points within coordinate
+        system
+        """
+        self.test_cube = PointLocation(lat=-2.5,lon=3.5,cube=self.stock_cube)
+        self.lat_coord = self.test_cube.find_point().coord('latitude')
+        self.lon_coord = self.test_cube.find_point().coord('longitude')
+        self.assertEqual([self.lat_coord.points[0], self.lon_coord.points[0]],
+                         [-2.0, 4.0])
+
+class TestAreaLocation(unittest.TestCase):
+
+    def setUp(self):
+        self.stock_cube = realistic_3d()
+        self.stock_cube.coord('grid_latitude').guess_bounds()
+        self.stock_cube.coord('grid_longitude').guess_bounds()
+
+    def test_find_area_general_case(self):
+        """
+        Tests general case of max/min definitions within coordinate system
+        """
+        self.test_cube = AreaLocation(lat_min=-2.92, lat_max = 1.63,
+                                       lon_min=0.35, lon_max = 3.89,
+                                       cube=self.stock_cube)
+        self.lat_coord = self.test_cube.find_area().coord('latitude')
+        self.lon_coord = self.test_cube.find_area().coord('longitude')
+        print(self.lat_coord.points[0], self.lon_coord.points[0])
+        self.assertEqual([self.lat_coord.points[0], self.lon_coord.points[0]],
+                         [-0.5, 2.0])
+
+    def test_find_area_on_boundary_case(self):
+        """
+        Tests case of max/min definitions on boundary between points within
+        coordinate system.
+        :return:
+        """
+        self.test_cube = AreaLocation(lat_min=-3.5, lat_max = 1.5,
+                                       lon_min=0.5, lon_max = 4.5,
+                                       cube=self.stock_cube)
+        self.lat_coord = self.test_cube.find_area().coord('latitude')
+        self.lon_coord = self.test_cube.find_area().coord('longitude')
+        print(self.lat_coord.points[0], self.lon_coord.points[0])
+        self.assertEqual([self.lat_coord.points[0], self.lon_coord.points[0]],
+                         [-0.5, 3.0])
+
+if __name__ == '__main__':
+    unittest.main()

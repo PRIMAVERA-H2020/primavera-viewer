0074b01860a875a38d7970850952f0d32faff031
Philip Rutter <rutter_100@outlook.com>
2018-07-20 14:33:29 +0100

    Ammended experiments_data.py so that all the cube formatting that takes place in the class ExperimentsData will be processed in parallel

--- primavera_viewer/experiments_data.py
+++ primavera_viewer/experiments_data.py
@@ -5,7 +5,8 @@ Module defines classes for subsetting and averaging multi-model/ensemble
 experiments as well as plotting functions to be used in the operation module.
 
 """
-
+from multiprocessing import Process, Manager
+import itertools
 import iris
 import numpy as np
 from primavera_viewer import (nearest_location as loc, cube_format as format,
@@ -57,22 +58,18 @@ class ExperimentsData:
                 min_longitude=self.location[2],
                 max_longitude=self.location[3])
 
-    def unify_spatial_coordinates(self):
+    def unify_spatial_coordinates(self, params, output):
         """
         Ensure that all spatial dimensions are defined by the same coordinate
         system: two 1D arrays of latitude and longitude coordinates.
         Method for redefining spatial coordinates can be specific to each model.
         :return: ExperimentData class with unified spatial coords
         """
-        all_cubes_list = iris.cube.CubeList([])
-        for cube in self.experiments_list:
-            cube = format.redefine_spatial_coords(cube)
-            all_cubes_list.append(cube)
-        self.experiments_list = all_cubes_list
-        return self
+        cube = params.get()
+        cube = format.redefine_spatial_coords(cube)
+        output.append(cube)
 
-
-    def constrain_location(self):
+    def constrain_location(self, params, output):
         """
         Purpose: Subsets cube location to set point in the coordinate system
         (CS). If self.location is a 2D array of latitude and longitude points a
@@ -82,20 +79,16 @@ class ExperimentsData:
         in the defined area and returning an area mean.
         :return: list of cubes at the requested point
         """
+        cube = params.get()
         if len(self.location) == 2:
-            all_cubes_list = iris.cube.CubeList([])
             latitude_point = self.location[0]
             longitude_point = self.location[1]
             print('Constraining location at point:\n' + str(latitude_point) +
                   'N ' + str(longitude_point) + 'E')
-            for cube in self.experiments_list:
-                cube = loc.PointLocation(latitude_point, longitude_point, cube)
-                cube = cube.find_point()
-                all_cubes_list.append(cube)
-            self.experiments_list = all_cubes_list
-            return self
+            cube = loc.PointLocation(latitude_point, longitude_point, cube)
+            cube = cube.find_point()
+            output.append(cube)
         if len(self.location) == 4:
-            all_cubes_list = iris.cube.CubeList([])
             latitude_min = self.location[0]
             latitude_max = self.location[1]
             longitude_min = self.location[2]
@@ -105,38 +98,54 @@ class ExperimentsData:
                   + str(latitude_max) + 'N\n'
                   'Longitude range: ' + str(longitude_min) + 'E to '
                   + str(longitude_max) + 'E')
-            for cube in self.experiments_list:
-                cube = loc.AreaLocation(latitude_min, latitude_max,
+            cube = loc.AreaLocation(latitude_min, latitude_max,
                                        longitude_min, longitude_max, cube)
-                cube = cube.find_area()
-                all_cubes_list.append(cube)
-            self.experiments_list = all_cubes_list
-            return self
+            cube = cube.find_area()
+            output.append(cube)
 
 
-    def unify_cube_format(self):
+    def unify_cube_format(self, params, output):
         """
         Ensure that all experiments have the same cube format i.e the same time
         coordinates and calendar, attributes, data type and auxillary coords.
         Time coordinate units and time point definitions can also be set.
         Example: units are set to be unified as 'days since 1950-01-01 00:00:00'
         and daily data is defined at the hour of midday.
-        :return:
         """
+        cube=params.get()
         print('Unifying formatting')
-        all_cubes_list = iris.cube.CubeList([])
-        for cube in self.experiments_list:
-            cube = format.change_calendar(cube,
-                                          new_units='days since 1950-01-01 '
-                                                    '00:00:00')
-            cube = format.add_extra_time_coords(cube)
-            cube = format.unify_data_type(cube)
-            cube = format.set_blank_attributes(cube)
-            cube = format.change_time_points(cube, hr=12)
-            cube = format.change_time_bounds(cube)
-            cube = format.remove_extra_time_coords(cube) # remove non-essential coord
-            all_cubes_list.append(cube)
-        self.experiments_list = all_cubes_list
+        cube = format.change_calendar(cube,
+                                      new_units='days since 1950-01-01 '
+                                                '00:00:00')
+        cube = format.add_extra_time_coords(cube)
+        cube = format.unify_data_type(cube)
+        cube = format.set_blank_attributes(cube)
+        cube = format.change_time_points(cube, hr=12)
+        cube = format.change_time_bounds(cube)
+        cube = format.remove_extra_time_coords(cube)# remove non-essential coord
+        output.append(cube)
+
+    def experiment_operations(self):
+
+        operations = [self.unify_spatial_coordinates, self.constrain_location,
+                      self.unify_cube_format]
+        for oper in operations:
+            max_simul_jobs = len(self.experiments_list)
+            jobs = []
+            manager = Manager()
+            params = manager.Queue()
+            result_list = manager.list()
+            for i in range(max_simul_jobs):
+                p = Process(target=oper, args=(params, result_list))
+                jobs.append(p)
+                p.start()
+            iters = itertools.chain(
+                self.experiments_list,(None,) * max_simul_jobs)
+            for iter in iters:
+                params.put(iter)
+            for j in jobs:
+                   j.join()
+            self.experiments_list = iris.cube.CubeList(list(result_list))
         return self
 
 

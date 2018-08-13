431c261835bbe21049e6d1e92c932bfaef2166eb
Philip Rutter <rutter_100@outlook.com>
2018-08-13 12:57:24 +0100

    Tools for handling single cube statistics

--- primavera_viewer/sim_statistics.py
+++ primavera_viewer/sim_statistics.py
@@ -1,17 +1,17 @@
 """
-Philip Rutter 10/07/18
-Module for single cube statistical methods such as annual/monthly/seasonal/area
-means
+Philip Rutter 13/08/18
+Module for single cube statistical methods such as annual/monthly means and
+anomalies.
 """
 
 import iris
 import iris.coord_categorisation as icc
 import numpy as np
-from primavera_viewer import exp_format as format
+from primavera_viewer import sim_format as format
 
 
 def annual_mean(cube):
-    print('getting annual mean '+cube.coord('experiment_label').points[0])
+    print('getting annual mean '+cube.coord('simulation_label').points[0])
     cube = format.add_extra_time_coords(cube)
     annual_mean_cube = cube.aggregated_by('year', iris.analysis.MEAN)
     annual_mean_cube.rename(annual_mean_cube.name() + '_annual_mean')
@@ -19,7 +19,7 @@ def annual_mean(cube):
 
 
 def monthly_analysis(cube):
-    print('getting monthly mean '+cube.coord('experiment_label').points[0])
+    print('getting monthly mean '+cube.coord('simulation_label').points[0])
     cube = format.add_extra_time_coords(cube)
     monthly_mean_cube = cube.aggregated_by(['month','year'], iris.analysis.MEAN)
     monthly_mean_cube.rename(monthly_mean_cube.name() + '_mean')
@@ -33,7 +33,7 @@ def monthly_analysis(cube):
 
 
 def daily_anomaly(cube):
-    print('getting daily anomaly '+cube.coord('experiment_label').points[0])
+    print('getting daily anomaly '+cube.coord('simulation_label').points[0])
     daily_mean_cube = format.add_extra_time_coords(cube)
     daily_mean_anomaly_list = iris.cube.CubeList([])
     all_months_mean = cube.aggregated_by(['month'], iris.analysis.MEAN)
@@ -53,7 +53,7 @@ def daily_anomaly(cube):
 
 def monthly_mean_anomaly(cube):
     print('getting monthly mean anomaly '
-          +cube.coord('experiment_label').points[0])
+          +cube.coord('simulation_label').points[0])
     cube = format.add_extra_time_coords(cube)
     monthly_mean_anomaly_list = iris.cube.CubeList([])
     all_months_mean = cube.aggregated_by(['month'], iris.analysis.MEAN)
@@ -77,7 +77,7 @@ def monthly_mean_anomaly(cube):
 
 def monthly_maximum_anomaly(cube):
     print('getting monthly maximum anomaly '
-          +cube.coord('experiment_label').points[0])
+          +cube.coord('simulation_label').points[0])
     cube = format.add_extra_time_coords(cube)
     monthly_max_anomaly_list = iris.cube.CubeList([])
     all_months_mean = cube.aggregated_by(['month'], iris.analysis.MEAN)
@@ -101,7 +101,7 @@ def monthly_maximum_anomaly(cube):
 
 def monthly_minimum_anomaly(cube):
     print('getting monthly minimum anomaly '
-          +cube.coord('experiment_label').points[0])
+          +cube.coord('simulation_label').points[0])
     cube = format.add_extra_time_coords(cube)
     monthly_min_anomaly_list = iris.cube.CubeList([])
     all_months_mean = cube.aggregated_by(['month'], iris.analysis.MEAN)
@@ -123,34 +123,34 @@ def monthly_minimum_anomaly(cube):
     return monthly_min_anomaly_list
 
 
-
-def seasonal_mean(cube):
-    seasons = ['winter','summer']
-    icc.add_season(cube, 'time', 'clim_season')
-    icc.add_season_year(cube, 'time', 'season_year')
-    season_mean_cube_list = iris.cube.CubeList([])
-    season_max_cube_list = iris.cube.CubeList([])
-    season_min_cube_list = iris.cube.CubeList([])
-    for season in seasons:
-        if season == 'winter':
-            months = 'djf'
-        if season == 'spring':
-            months = 'mam'
-        if season == 'summer':
-            months = 'jja'
-        if season == 'autumn':
-            months = 'son'
-        single_season_cube = cube.extract(iris.Constraint(clim_season=months))
-        season_mean_cube = single_season_cube.aggregated_by(
-            ['clim_season', 'season_year'], iris.analysis.MEAN)
-        season_mean_cube.rename(season_mean_cube.name() + '_'+season+'_mean')
-        season_mean_cube_list.append(season_mean_cube)
-        season_max = single_season_cube.aggregated_by(
-            ['clim_season', 'season_year'], iris.analysis.MAX)
-        season_max.rename(season_max.name() + '_'+season+'_max')
-        season_max_cube_list.append(season_max)
-        season_min = single_season_cube.aggregated_by(
-            ['clim_season', 'season_year'], iris.analysis.MIN)
-        season_min.rename(season_min.name() + '_'+season+'_min')
-        season_min_cube_list.append(season_min)
-    return [season_mean_cube_list,season_max_cube_list,season_min_cube_list]
+# SEASONAL MEAN
+# def seasonal_mean(cube):
+#     seasons = ['winter','summer']
+#     icc.add_season(cube, 'time', 'clim_season')
+#     icc.add_season_year(cube, 'time', 'season_year')
+#     season_mean_cube_list = iris.cube.CubeList([])
+#     season_max_cube_list = iris.cube.CubeList([])
+#     season_min_cube_list = iris.cube.CubeList([])
+#     for season in seasons:
+#         if season == 'winter':
+#             months = 'djf'
+#         if season == 'spring':
+#             months = 'mam'
+#         if season == 'summer':
+#             months = 'jja'
+#         if season == 'autumn':
+#             months = 'son'
+#         single_season_cube = cube.extract(iris.Constraint(clim_season=months))
+#         season_mean_cube = single_season_cube.aggregated_by(
+#             ['clim_season', 'season_year'], iris.analysis.MEAN)
+#         season_mean_cube.rename(season_mean_cube.name() + '_'+season+'_mean')
+#         season_mean_cube_list.append(season_mean_cube)
+#         season_max = single_season_cube.aggregated_by(
+#             ['clim_season', 'season_year'], iris.analysis.MAX)
+#         season_max.rename(season_max.name() + '_'+season+'_max')
+#         season_max_cube_list.append(season_max)
+#         season_min = single_season_cube.aggregated_by(
+#             ['clim_season', 'season_year'], iris.analysis.MIN)
+#         season_min.rename(season_min.name() + '_'+season+'_min')
+#         season_min_cube_list.append(season_min)
+#     return [season_mean_cube_list,season_max_cube_list,season_min_cube_list]

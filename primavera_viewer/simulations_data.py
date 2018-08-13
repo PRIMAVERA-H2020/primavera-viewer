63c014712792c66a55d954e3b6697546a9ed8603
Philip Rutter <rutter_100@outlook.com>
2018-08-13 12:39:23 +0100

    Operation to unify datasets and get simulations mean

--- primavera_viewer/simulations_data.py
+++ primavera_viewer/simulations_data.py
@@ -1,43 +1,43 @@
 """
 Philip Rutter 11/07/18
 
-Module defines a class 'ExperimentsData' used to manipulating and handling data
-from experiments once fully loaded and concatenated.
+Module defines a class 'SimulationsData' used to manipulating and handling data
+from simulations once fully loaded and concatenated.
 """
 from multiprocessing import Process, Manager
 import itertools
 import iris
 import numpy as np
-from primavera_viewer import (nearest_location as loc, exp_format as format)
+from primavera_viewer import (nearest_location as loc, sim_format as format)
 from datetime import datetime
 
-class ExperimentsData:
+class SimulationsData:
     """
-    Class containing all experiment data and the requested location. Methods
+    Class containing all simulation data and the requested location. Methods
     within class are used to manipulate spatial coordinates, time dimensions
-    and cube formatting in order to accurately plot and compare experiments.
+    and cube formatting in order to accurately plot and compare simulations.
 
     Note: All calendars are converted to a 360 day calendar by this script.
     Gregorian and 365 day calendars keep 31/01 and 31/03 to balance 28/02.
     All other 31st days and leap days are removed.
     """
-    def __init__(self, exp_list=iris.cube.CubeList([]), loc=np.array([]),
+    def __init__(self, sim_list=iris.cube.CubeList([]), loc=np.array([]),
                  t_constr=np.array([])):
-        self.experiments_list = exp_list
+        self.simulations_list = sim_list
         self.location = loc
         self.time_constraints = t_constr
 
     def __repr__(self):
         if len(self.location) == 2:
-            return '{experiments_list}\n' \
+            return '{simulations_list}\n' \
                    '{latitude}N {longitude}E'.format(
-                experiments_list=self.experiments_list,
+                simulations_list=self.simulations_list,
                 latitude=self.location[0],
                 longitude=self.location[1])
         if len(self.location) == 4:
-            return '{experiments_list}\n{min_latitude}N to {max_latitude}N\n' \
+            return '{simulations_list}\n{min_latitude}N to {max_latitude}N\n' \
                    '{min_longitude}E to {max_longitude}E'.format(
-                experiments_list=self.experiments_list,
+                simulations_list=self.simulations_list,
                 min_latitude=self.location[0],
                 max_latitude=self.location[1],
                 min_longitude=self.location[2],
@@ -45,18 +45,18 @@ class ExperimentsData:
 
     def __str__(self):
         if len(self.location) == 2:
-            return 'Experiments List:\n{experiments_list}\n' \
+            return 'Simulations List:\n{simulations_list}\n' \
                    'Location:\n{latitude}N {longitude}E'.format(
-                experiments_list=self.experiments_list,
+                simulations_list=self.simulations_list,
                 latitude=self.location[0],
                 longitude=self.location[1])
         if len(self.location) == 4:
-            return 'Experiments List:\n{experiments_list}\n' \
+            return 'Simulations List:\n{simulations_list}\n' \
                    'Location:\nLatitude range: {min_latitude}N ' \
                    'to {max_latitude}N\n' \
                    'Longitude range: {min_longitude}E ' \
                    'to {max_longitude}E'.format(
-                experiments_list=self.experiments_list,
+                simulations_list=self.simulations_list,
                 min_latitude=self.location[0],
                 max_latitude=self.location[1],
                 min_longitude=self.location[2],
@@ -86,7 +86,7 @@ class ExperimentsData:
             latitude_point = self.location[0]
             longitude_point = self.location[1]
             print('Constraining location for '
-                  +cube.coord('experiment_label').points[0]+' at point:\n'
+                  +cube.coord('simulation_label').points[0]+' at point:\n'
                   +str(latitude_point)+'N '+str(longitude_point)+'E')
             cube = loc.PointLocation(latitude_point, longitude_point, cube)
             cube = cube.find_point()
@@ -97,7 +97,7 @@ class ExperimentsData:
             longitude_min = self.location[2]
             longitude_max = self.location[3]
             print('Constraining location for '
-                  +cube.coord('experiment_label').points[0]+' over region:\n' 
+                  +cube.coord('simulation_label').points[0]+' over region:\n' 
                   'Latitude range: '+str(latitude_min)+'N to '
                   +str(latitude_max)+'N\n'
                   'Longitude range: '+str(longitude_min)+'E to '
@@ -109,7 +109,7 @@ class ExperimentsData:
 
     def unify_cube_format(self, params, output, time_constr):
         """
-        Ensure that all experiments have the same cube format i.e the same time
+        Ensure that all simulations have the same cube format i.e the same time
         coordinates and calendar, attributes, data type and auxillary coords.
         Time coordinate units and time point definitions can also be set.
         Example: units are currently set to be unified as
@@ -118,7 +118,7 @@ class ExperimentsData:
         """
         cube=params.get()
         print('Unifying formatting for '
-              +cube.coord('experiment_label').points[0])
+              +cube.coord('simulation_label').points[0])
         cube = format.change_calendar(cube, time_constr,
                                       new_units='days since 1950-01-01 '
                                                 '00:00:00')
@@ -131,15 +131,15 @@ class ExperimentsData:
         output.append(cube)
 
 
-    def experiments_operations(self):
+    def simulations_operations(self):
         """
-        Perform all the above operations in parallel for each experiemnt the
+        Perform all the above operations in parallel for each simulation the
         user wishes to compare.
         """
         operations = ['unifying spatial coords', 'constraining location',
                       'unifying cube format']
         for oper in operations:
-            if self.experiments_list[0].has_lazy_data():
+            if self.simulations_list[0].has_lazy_data():
                 print('Before '+oper+': cubes have lazy data')
             else:
                 print('Before '+oper+': no lazy data')
@@ -157,40 +157,36 @@ class ExperimentsData:
             if oper == 'unifying cube format':
                 func = self.unify_cube_format
                 arguments = (params, result_list, self.time_constraints)
-            max_simul_jobs = len(self.experiments_list)
+            max_simul_jobs = len(self.simulations_list)
             for i in range(max_simul_jobs):
                 p = Process(target=func, args=arguments)
                 jobs.append(p)
                 p.start()
             iters = itertools.chain(
-                self.experiments_list,(None,) * max_simul_jobs)
+                self.simulations_list,(None,) * max_simul_jobs)
             for iter in iters:
                 params.put(iter)
             for j in jobs:
                    j.join()
-            self.experiments_list = iris.cube.CubeList(list(result_list))
+            self.simulations_list = iris.cube.CubeList(list(result_list))
             entime = datetime.now()
             print('Time ellapsed when ' + oper + ': ' + str(entime - sttime))
-            if self.experiments_list[0].has_lazy_data():
+            if self.simulations_list[0].has_lazy_data():
                 print('After '+oper+': cubes have lazy data\n')
             else:
                 print('After '+oper+': no lazy data\n')
         return self
 
 
-    def all_experiments_mean(self):
+    def all_simulations_mean(self):
         """
-        Calculates the multi-experiment mean from the cube list of fully unified
-        experiments data above.
+        Calculates the multi-simulation mean from the cube list of fully unified
+        simulations data above.
         """
-        if len(self.experiments_list) > 1:
+        if len(self.simulations_list) > 1:
             sttime = datetime.now()
-            if self.experiments_list[0].has_lazy_data():
-                print('Before calculating mean: cubes have lazy data')
-            else:
-                print('Before calculating mean: no lazy data')
             all_cubes_list = iris.cube.CubeList([])
-            for cube in self.experiments_list:
+            for cube in self.simulations_list:
                 try:
                     cube.remove_coord('latitude')
                 except:
@@ -202,15 +198,12 @@ class ExperimentsData:
                 all_cubes_list.append(cube)
             cubes = all_cubes_list
             merged_cube = cubes.merge_cube()
-            experiments_mean = merged_cube.collapsed('experiment_label',
+            simulations_mean = merged_cube.collapsed('simulation_label',
                                                      iris.analysis.MEAN)
-            experiments_mean.coord('experiment_label').points[0]='Experiments Mean'
+            simulations_mean.coord(
+                'simulation_label').points[0]='Simulations Mean'
             entime = datetime.now()
-            print('Time ellapsed when calculating mean: ' + str(entime - sttime))
-            if self.experiments_list[0].has_lazy_data():
-                print('After calculating mean: cubes have lazy data\n')
-            else:
-                print('After calculating mean: no lazy data\n')
-            return experiments_mean
+            print('Time ellapsed when calculating mean: '+str(entime-sttime))
+            return simulations_mean
         else:
             return iris.cube.Cube([])

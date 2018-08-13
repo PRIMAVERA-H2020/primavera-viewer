cb2fe2e7da9bed66fbd281ad0d250f12bcc324d7
Philip Rutter <rutter_100@outlook.com>
2018-08-13 12:30:36 +0100

    Reading, loading and concatenating datasets

--- primavera_viewer/simulations_loading.py
+++ primavera_viewer/simulations_loading.py
@@ -11,52 +11,50 @@ from multiprocessing import Process, Manager
 import itertools
 import iris
 import numpy as np
-from primavera_viewer.exp_format import (add_experiment_label,
+from primavera_viewer.sim_format import (add_simulation_label,
                                          change_time_units)
 from datetime import datetime
-startTime = datetime.now()
 
+# Ignore warnings displayed when loading data
 warnings.filterwarnings("ignore")
 
-print('get json')
-FILENAME = 'app_config.json'
+# Load in json file containing data dirs
+FILENAME = '../app_config.json'
 # Read the config file in
 with open(FILENAME) as fh:
     app_config = json.load(fh)
-print('got json\n')
 
-
-class ExperimentsLoading:
+class SimulationsLoading:
     """
-    A class of experiments defined by a model and an ensemble member at a given
-    resolution for a single variable. Data for each experiment can be loaded,
+    A class of simulations for a given variable defined by a model and ensemble
+    member and a time constraint. Data for each simulation can be loaded,
     constrained in time and concatenated into a single cube to be held in a
     output cube list.
 
     Paths to each directory containing data can be altered in the json file
     'app_config.json'. Each pathway is linked to the corresponding CMIP6 data
-    reference syntax.
+    reference syntax (DRS).
     """
     def __init__(self, var=list(), mod=list(), ens=list(), constr=np.array([])):
         self.variable = var
         self.models = mod
         self.ensembles = ens
         self.constraints = constr
-        self.experiments_list = list()
+        self.simulations_list = list()
         for v in self.variable:
             for m in self.models:
                 for e in self.ensembles:
+                    variable = str(v)
                     model = str(m)
                     ensemble = str(e)
-                    variable = str(v)
                     data_required = 'CMIP6.HighResMIP.'+model+\
                                     '.highresSST-present.'+ensemble+'.day.'\
                                     +variable
+                    # Create list of available simulations given data exists
                     try:
                         dir = app_config[data_required]['directory']
-                        experiment = [model, ensemble, variable]
-                        # create a list of available experiments data
-                        self.experiments_list.append(experiment)
+                        simulation = [model, ensemble, variable]
+                        self.simulations_list.append(simulation)
                     except:
                         print('ERROR: directory for '+model+'.'+ensemble+'.'
                               +variable+' does not exist')
@@ -75,16 +73,16 @@ class ExperimentsLoading:
         self.constraints = constr
 
     def __repr__(self):
-        return 'Experiments:\n{experiments}'.format(
-            experiments = self.experiments_list)
+        return 'Simulations:\n{simulations}'.format(
+            simulations = self.simulations_list)
 
     def __str__(self):
-        return 'Experiments:\n{experiments}'.format(
-            experiments = self.experiments_list)
+        return 'Simulations:\n{simulations}'.format(
+            simulations = self.simulations_list)
 
     def concatenate_data(self, cubes):
         """
-        Concatenates data for a single experiment.
+        Concatenates data for a single simulation.
         """
         attributes = ['creation_date', 'history', 'tracking_id', 'realm']
         for cube in cubes:
@@ -97,42 +95,42 @@ class ExperimentsLoading:
 
     def load_data(self, params, output):
         """
-        Loads data with defined constraints on time (year) for single experiment
+        Loads data with defined constraints on time (year) for single simulation
         assuming data directory can be found in the .json file.
         """
-        experiment = params.get()
+        simulation = params.get()
         # constrain over the required time
         constraints = iris.Constraint(time=lambda cell: self.constraints[0]
                                                         <= cell.point.year <
                                                         self.constraints[1])
-        data_required = 'CMIP6.HighResMIP.'+experiment[0]+\
-                        '.highresSST-present.'+experiment[1]+'.day.'\
-                        +experiment[2]
+        data_required = 'CMIP6.HighResMIP.'+simulation[0]+\
+                        '.highresSST-present.'+simulation[1]+'.day.'\
+                        +simulation[2]
         dir = app_config[data_required]['directory']
-        print('Loading '+experiment[2]+' data for model ensemble '+experiment[0]
-              +' '+experiment[1])
-        cubes = iris.load(dir + '*.nc', constraints)
-        print(cubes)
+        print('Loading '+simulation[2]+' data for model ensemble '+simulation[0]
+              +' '+simulation[1])
+        cubes = iris.load(dir + '/*.nc', constraints)
         cubes_diff_units = iris.cube.CubeList([])
         for cube in cubes:
             cube = change_time_units(cube, 'days since 1950-01-01 00:00:00')
             cubes_diff_units.append(cube)
+        # Concatenate data if file structure requires
         if len(cubes) > 1:
             cube = self.concatenate_data(cubes_diff_units)
         else:
             cube = cubes[0]
-        # Add an aux coord unique to each experiment
-        cube = add_experiment_label(cube)
+        # Add an aux coord unique to each simulation
+        cube = add_simulation_label(cube)
         output.append(cube)
 
     def load_all_data(self):
         """
-        Loads data all experiments in self.experiments_list in parallel.
-        Returns: cube list with single cube per experiment
+        Loads data all simulations in self.simulations_list in parallel.
+        Returns: cube list with single cube per simulation
         """
         sttime = datetime.now()
         print('Starting loading all at: '+str(sttime))
-        max_simul_jobs = len(self.experiments_list)
+        max_simul_jobs = len(self.simulations_list)
         jobs = []
         manager = Manager()
         params = manager.Queue()
@@ -141,7 +139,7 @@ class ExperimentsLoading:
             p = Process(target=self.load_data, args=(params, result_list))
             jobs.append(p)
             p.start()
-        iters = itertools.chain(self.experiments_list,(None,) * max_simul_jobs)
+        iters = itertools.chain(self.simulations_list,(None,) * max_simul_jobs)
         for iter in iters:
             params.put(iter)
         for j in jobs:
@@ -149,9 +147,4 @@ class ExperimentsLoading:
         cube_list = iris.cube.CubeList(list(result_list))
         entime = datetime.now()
         print('Finished loading all at: '+str(entime))
-        print('Time ellapsed during load: '+str(entime - sttime)+'\n')
-        if cube_list[0].has_lazy_data():
-            print('After loading: cubes have lazy data\n')
-        else:
-            print('After loading: no lazy data\n')
         return cube_list
